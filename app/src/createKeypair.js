718386017bcc320ae698e9db9bf1c6129e1d64b6
Panasun Sunanta <panasunsunanta@gmail.com>
2021-10-25 18:04:59 +0700

    feat: refactor

--- app/src/createKeypair.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const anchor = require("@project-serum/anchor");
-const web3 = require('@solana/web3.js')
-const account = anchor.web3.Keypair.generate();
-
-fs.writeFileSync('./src/keypair.json', JSON.stringify(account))
