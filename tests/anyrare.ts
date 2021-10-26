3441bb049c325d2e58f5133b99ae4fbf6a1fc2c2
Panasun Sunanta <panasunsunanta@gmail.com>
2021-10-27 00:16:50 +0700

    feat: add proxy_token

--- tests/anyrare.ts
+++ tests/anyrare.ts
@@ -1,111 +1,46 @@
 import * as anchor from "@project-serum/anchor";
-import * as serumCmn from "@project-serum/common";
-import {TokenInstructions} from "@project-serum/serum";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { assert } from "chai";
 
-describe("cahiers-check", () => {
+describe("Token Proxy", () => {
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
-  const program = anchor.workspace.CashiersCheck;
+  const program = anchor.workspace.TokenProxy;
 
-  let mint = null;
-  let from = null;
-  let to = null;
+  const mint = anchor.web3.Keypair.generate();
+  const account = anchor.web3.Keypair.generate();
+  const authority = anchor.web3.Keypair.generate();
+  const rent = anchor.web3.Keypair.generate();
 
-  it("Sets up initial test state", async () => {
-    mint = await createMint(provider, undefined);
-    from = await createTokenAccount(provider, mint, provider.wallet.publicKey);
-    to = await createTokenAccount(provider, mint, provider.wallet.publicKey);
-    console.log('mint', mint);
-    console.log('from', from);
-    
-    const result = await program.rpc.createToken({
-      accounts: {
-        authority: provider.wallet.publicKey,
-        mint,
-        to: from,
-        tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID
+  it("proxyInitializeMint", async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(authority.publicKey, 2000000000),
+      "confirmed"
+    );
+    const sleep = t => new Promise(s => setTimeout(s, t));
+    await sleep(10000);
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(rent.publicKey, 2000000000),
+      "confirmed"
+    );
+
+    const result = await program.rpc.proxyInitializeMint(
+      0,
+      authority.publicKey,
+      {
+        accounts: {
+          authority: authority.publicKey,
+          account: account.publicKey,
+          mint: mint.publicKey,
+          rent: rent.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID
+        },
+        signers: [authority],
       }
-    });
+    );
 
     console.log(result);
 
   });
-
 });
-
-async function createMint(provider, authority) {
-  if (authority === undefined) {
-    authority = provider.wallet.publicKey;
-  }
-  const mint = anchor.web3.Keypair.generate();
-  const instructions = await serumCmn.createMintInstructions(
-    provider,
-    authority,
-    mint.publicKey
-  );
-
-  const tx = new anchor.web3.Transaction();
-  tx.add(...instructions);
-
-  await provider.send(tx, [mint]);
-
-  return mint.publicKey;
-}
-
-async function createMintInstructions(provider, authority, mint) {
-  let instructions = [
-    anchor.web3.SystemProgram.createAccount({
-      fromPubkey: provider.wallet.publicKey,
-      newAccountPubkey: mint,
-      space: 82,
-      lamports: await provider.connection.getMinimumBalanceForRentExemption(82),
-      programId: TOKEN_PROGRAM_ID,
-    }),
-    TokenInstructions.initializeMint({
-      mint,
-      decimals: 0,
-      mintAuthority: authority,
-    })
-  ];
-
-  return instructions;
-}
-
-async function createTokenAccount(provider, mint, owner) {
-  const vault = anchor.web3.Keypair.generate();
-  const tx = new anchor.web3.Transaction();
-  tx.add(
-    ...(await createTokenAccountInstrs(provider, vault.publicKey, mint, owner, undefined))
-  );
-  await provider.send(tx, [vault]);
-  return vault.publicKey;
-}
-
-async function createTokenAccountInstrs(
-  provider,
-  newAccountPubkey,
-  mint,
-  owner,
-  lamports
-) {
-  if (lamports === undefined) {
-    lamports = await provider.connection.getMinimumBalanceForRentExemption(165);
-  }
-  return [
-    anchor.web3.SystemProgram.createAccount({
-      fromPubkey: provider.wallet.publicKey,
-      newAccountPubkey,
-      space: 165,
-      lamports,
-      programId: TOKEN_PROGRAM_ID,
-    }),
-    TokenInstructions.initializeAccount({
-      account: newAccountPubkey,
-      mint,
-      owner
-    })
-  ]
-}
