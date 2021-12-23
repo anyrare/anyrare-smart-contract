c3d1fea374b813bd25e6e18c8b31c7f317bafb6f
Panasun Sunanta <panasunsunanta@gmail.com>
2021-12-23 12:59:51 +0700

    fix: emacs ignore

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -1,182 +1,153 @@
-pragma solidity ^0.8.0;
+pragma solidity ^ 0.8.0;
 pragma abicoder v2;
 
 contract Governance {
-    struct Manager {
-        address addr;
-        uint32 controlWeight;
-        uint32 maxWeight;
+  struct Manager {
+    address addr;
+    uint32 controlWeight;
+    uint32 maxWeight;
+  }
+
+  struct Auditor {
+    bool approve;
+  }
+
+  struct Custodian {
+    bool approve;
+  }
+
+  struct Policy {
+    uint32 policyWeight;
+    uint32 maxWeight;
+    uint32 voteDurationSecond;
+    uint32 minWeightOpenVote;
+    uint32 minWeightValidVote;
+    uint32 minWeightApproveVote;
+    bool exists;
+    bool openVote;
+    address currentProposal;
+  }
+
+  struct Voter {
+    bool voted;
+    bool approve;
+  }
+
+  address public memberContract;
+  address public auctionContract;
+  address public collectionContract;
+  address public ARATokenContract;
+  address public proposalContract;
+
+  mapping(bytes8 = > Policy) public policies;
+  mapping(uint16 = > Manager) public managers;
+  mapping(address = > Auditor) public auditors;
+  mapping(address = > Custodian) public custodians;
+
+  uint16 public totalManager;
+
+  constructor() public {
+    Policy storage collateralWeight =
+        policies[stringToBytes8("COLLATERAL_WEIGHT")];
+    collateralWeight.policyWeight = 400000;
+    collateralWeight.maxWeight = 1000000;
+
+    Manager storage m0 = managers[0];
+    m0.addr = address(this);
+    m0.controlWeight = 150;
+    m0.maxWeight = 1000;
+    totalManager = 1;
+  }
+
+  function stringToBytes8(string memory str) public pure returns(bytes8) {
+    bytes8 temp = 0x0;
+    assembly {
+    temp:
+      = mload(add(str, 32))
     }
+    return temp;
+  }
 
-    struct Auditor {
-        bool approve;
+  function setMemberContract(address _memberContract) public {
+    memberContract = _memberContract;
+  }
+
+  function getARATokenContract() public view returns(address) {
+    return ARATokenContract;
+  }
+
+  function getMemberContract() public view returns(address) {
+    return memberContract;
+  }
+
+  function getManager(uint16 index) public view returns(
+      Manager memory manager) {
+    return managers[index];
+  }
+
+  function getTotalManager() public view returns(uint16) {
+    return totalManager;
+  }
+
+  function getPolicy(string memory policyName) public view returns(
+      Policy memory policy) {
+    return policies[stringToBytes8(policyName)];
+  }
+
+  function getPolicyByIndex(bytes8 policyIndex) public view returns(
+      Policy memory policy) {
+    return policies[policyIndex];
+  }
+
+  function isManager(address addr) public returns(bool) {
+    for (uint16 i = 0; i < totalManager; i++) {
+      if (managers[i].addr == addr && addr != address(0x0)) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    struct Custodian {
-        bool approve;
-    }
+  function initPolicy(string memory policyName, uint32 policyWeight,
+                      uint32 maxWeight, uint32 voteDurationSecond,
+                      uint32 minWeightOpenVote, uint32 minWeightValidVote,
+                      uint32 minWeightApproveVote) public {
+    require(isManager(msg.sender), "Error 3000: No permission to init policy.");
 
-    struct Policy {
-        uint32 policyWeight;
-        uint32 maxWeight;
-        uint32 voteDurationSecond;
-        uint32 minWeightOpenVote;
-        uint32 minWeightValidVote;
-        uint32 minWeightApproveVote;
-        bool exists;
-        bool openVote;
-        address currentProposal;
-    }
+    bytes8 policyIndex = stringToBytes8(policyName);
+    require(!policies[policyIndex].exists,
+            "Error 3001: This policy already exists.");
 
-    struct Voter {
-        bool voted;
-        bool approve;
-    }
+    Policy storage p = policies[policyIndex];
+    p.exists = true;
+    p.policyWeight = policyWeight;
+    p.maxWeight = maxWeight;
+    p.voteDurationSecond = voteDurationSecond;
+    p.minWeightOpenVote = minWeightOpenVote;
+    p.minWeightValidVote = minWeightValidVote;
+    p.minWeightApproveVote = minWeightApproveVote;
+    p.openVote = false;
+  }
 
-    address public memberContract;
-    address public auctionContract;
-    address public collectionContract;
-    address public ARATokenContract;
-    address public proposalContract;
+  function setPolicyByProposal(
+      bytes8 policyIndex, address proposalAddress, uint32 policyWeight,
+      uint32 maxWeight, uint32 voteDurationSecond, uint32 minWeightOpenVote,
+      uint32 minWeightValidVote, uint32 minWeightApproveVote) public {
+    require(msg.sender == proposalContract,
+            "Error 3002: No permission to set policy.");
 
-    mapping(bytes8 => Policy) public policies;
-    mapping(uint16 => Manager) public managers;
-    mapping(address => Auditor) public auditors;
-    mapping(address => Custodian) public custodians;
+    Policy storage p = policies[policyIndex];
 
-    uint16 public totalManager;
+    require(proposalAddress == p.currentProposal,
+            "Error 3003: Invalid proposal address.");
 
-    constructor() public {
-        Policy storage collateralWeight = policies[
-            stringToBytes8("COLLATERAL_WEIGHT")
-        ];
-        collateralWeight.policyWeight = 400000;
-        collateralWeight.maxWeight = 1000000;
-
-        Manager storage m0 = managers[0];
-        m0.addr = address(this);
-        m0.controlWeight = 150;
-        m0.maxWeight = 1000;
-        totalManager = 1;
-    }
-
-    function stringToBytes8(string memory str) public pure returns (bytes8) {
-        bytes8 temp = 0x0;
-        assembly {
-            temp := mload(add(str, 32))
-        }
-        return temp;
-    }
-
-    function setMemberContract(address _memberContract) public {
-        memberContract = _memberContract;
-    }
-
-    function getARATokenContract() public view returns (address) {
-        return ARATokenContract;
-    }
-
-    function getMemberContract() public view returns (address) {
-        return memberContract;
-    }
-
-    function getManager(uint16 index)
-        public
-        view
-        returns (Manager memory manager)
-    {
-        return managers[index];
-    }
-
-    function getTotalManager() public view returns (uint16) {
-        return totalManager;
-    }
-
-    function getPolicy(string memory policyName)
-        public
-        view
-        returns (Policy memory policy)
-    {
-        return policies[stringToBytes8(policyName)];
-    }
-
-    function getPolicyByIndex(bytes8 policyIndex)
-        public
-        view
-        returns (Policy memory policy)
-    {
-        return policies[policyIndex];
-    }
-
-    function isManager(address addr) public returns (bool) {
-        for (uint16 i = 0; i < totalManager; i++) {
-            if (managers[i].addr == addr && addr != address(0x0)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function initPolicy(
-        string memory policyName,
-        uint32 policyWeight,
-        uint32 maxWeight,
-        uint32 voteDurationSecond,
-        uint32 minWeightOpenVote,
-        uint32 minWeightValidVote,
-        uint32 minWeightApproveVote
-    ) public {
-        require(
-            isManager(msg.sender),
-            "Error 3000: No permission to init policy."
-        );
-
-        bytes8 policyIndex = stringToBytes8(policyName);
-        require(
-            !policies[policyIndex].exists,
-            "Error 3001: This policy already exists."
-        );
-
-        Policy storage p = policies[policyIndex];
-        p.exists = true;
-        p.policyWeight = policyWeight;
-        p.maxWeight = maxWeight;
-        p.voteDurationSecond = voteDurationSecond;
-        p.minWeightOpenVote = minWeightOpenVote;
-        p.minWeightValidVote = minWeightValidVote;
-        p.minWeightApproveVote = minWeightApproveVote;
-        p.openVote = false;
-    }
-
-    function setPolicyByProposal(
-        bytes8 policyIndex,
-        address proposalAddress,
-        uint32 policyWeight,
-        uint32 maxWeight,
-        uint32 voteDurationSecond,
-        uint32 minWeightOpenVote,
-        uint32 minWeightValidVote,
-        uint32 minWeightApproveVote
-    ) public {
-        require(
-            msg.sender == proposalContract,
-            "Error 3002: No permission to set policy."
-        );
-
-        Policy storage p = policies[policyIndex];
-
-        require(
-            proposalAddress == p.currentProposal,
-            "Error 3003: Invalid proposal address."
-        );
-
-        p.policyWeight = policyWeight;
-        p.maxWeight = maxWeight;
-        p.voteDurationSecond = voteDurationSecond;
-        p.minWeightOpenVote = minWeightOpenVote;
-        p.minWeightValidVote = minWeightValidVote;
-        p.minWeightApproveVote = minWeightApproveVote;
-        p.openVote = false;
-        p.currentProposal = address(0x0);
-    }
+    p.policyWeight = policyWeight;
+    p.maxWeight = maxWeight;
+    p.voteDurationSecond = voteDurationSecond;
+    p.minWeightOpenVote = minWeightOpenVote;
+    p.minWeightValidVote = minWeightValidVote;
+    p.minWeightApproveVote = minWeightApproveVote;
+    p.openVote = false;
+    p.currentProposal = address(0x0);
+  }
 }
--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -1,167 +1,163 @@
-pragma solidity ^0.8.0;
+pragma solidity ^ 0.8.0;
 pragma abicoder v2;
 
 import "./ARAToken.sol";
 import "./Governance.sol";
 
 contract Proposal {
-    struct Voter {
-        bool voted;
-        bool approve;
-    }
+  struct Voter {
+    bool voted;
+    bool approve;
+  }
 
-    struct PolicyProposal {
-        bytes8 policyIndex;
-        bool exists;
-        bool openVote;
-        uint256 closeVoteTimestamp;
-        uint32 policyWeight;
-        uint32 maxWeight;
-        uint32 voteDurationSecond;
-        uint32 minWeightOpenVote;
-        uint32 minWeightValidVote;
-        uint32 minWeightApproveVote;
-        uint256 totalVoteToken;
-        uint256 totalApproveToken;
-        uint256 totalSupplyToken;
-        bool voteResult;
-        uint256 processResultTimestamp;
-        uint256 totalVoter;
-        mapping(uint256 => address) votersAddress;
-        mapping(address => Voter) voters;
-    }
+  struct PolicyProposal {
+    bytes8 policyIndex;
+    bool exists;
+    bool openVote;
+    uint256 closeVoteTimestamp;
+    uint32 policyWeight;
+    uint32 maxWeight;
+    uint32 voteDurationSecond;
+    uint32 minWeightOpenVote;
+    uint32 minWeightValidVote;
+    uint32 minWeightApproveVote;
+    uint256 totalVoteToken;
+    uint256 totalApproveToken;
+    uint256 totalSupplyToken;
+    bool voteResult;
+    uint256 processResultTimestamp;
+    uint256 totalVoter;
+    mapping(uint256 = > address) votersAddress;
+    mapping(address = > Voter) voters;
+  }
 
-    struct ManagerInfo {
-        address addr;
-        uint32 controlWeight;
-        uint32 maxWeight;
-    }
+  struct ManagerInfo {
+    address addr;
+    uint32 controlWeight;
+    uint32 maxWeight;
+  }
 
-    struct ManagerProposal {
-        bytes8 policyIndex;
-        bool exists;
-        bool openVote;
-        uint256 closeVoteTimestamp;
-        uint256 totalVoteToken;
-        uint256 totalApproveToken;
-        uint256 totalSupplyToken;
-        bool voteResult;
-        uint256 processResultTimestamp;
-        uint256 totalVoter;
-        uint16 totalManager;
-        mapping(uint256 => address) votersAddress;
-        mapping(address => Voter) voters;
-        mapping(uint16 => ManagerInfo) managers;
-    }
-       
-    address GovernanceContract;
+  struct ManagerProposal {
+    bytes8 policyIndex;
+    bool exists;
+    bool openVote;
+    uint256 closeVoteTimestamp;
+    uint256 totalVoteToken;
+    uint256 totalApproveToken;
+    uint256 totalSupplyToken;
+    bool voteResult;
+    uint256 processResultTimestamp;
+    uint256 totalVoter;
+    uint16 totalManager;
+    mapping(uint256 = > address) votersAddress;
+    mapping(address = > Voter) voters;
+    mapping(uint16 = > ManagerInfo) managers;
+  }
 
-    mapping(address => PolicyProposal) proposals;
+  address GovernanceContract;
 
-    function openProposal(
-        string memory policyName,
-        address addr,
-        uint32 policyWeight,
-        uint32 maxWeight,
-        uint32 voteDurationSecond,
-        uint32 minWeightOpenVote,
-        uint32 minWeightValidVote,
-        uint32 minWeightApproveVote
-    ) public {
-        require(
-            !proposals[addr].exists,
-            "Error 4000: Proposal address already exists."
-        );
+  mapping(address = > PolicyProposal) policyProposals;
+  mapping(address = > ManagerProposal) managerProposals;
 
-        Governance g = Governance(GovernanceContract);
-        ERC20 t = ERC20(g.getARATokenContract());
-        bytes8 policyIndex = g.stringToBytes8(policyName);
+  function openPolicyProposal(
+      string memory policyName, address addr, uint32 policyWeight,
+      uint32 maxWeight, uint32 voteDurationSecond, uint32 minWeightOpenVote,
+      uint32 minWeightValidVote, uint32 minWeightApproveVote) public {
+    require(!policyProposals[addr].exists,
+            "Error 4000: Policy proposal address already exists.");
 
-        require(
-            t.balanceOf(msg.sender) >=
+    Governance g = Governance(GovernanceContract);
+    ERC20 t = ERC20(g.getARATokenContract());
+    bytes8 policyIndex = g.stringToBytes8(policyName);
+
+    require(t.balanceOf(msg.sender) >=
                 (t.totalSupply() * g.getPolicy(policyName).minWeightOpenVote) /
                     g.getPolicy(policyName).maxWeight,
-            "Error 4001: Insufficient token to open proposal."
-        );
+            "Error 4001: Insufficient token to open policy proposal.");
 
-        PolicyProposal storage p = proposals[addr];
-        p.policyIndex = policyIndex;
-        p.exists = true;
-        p.openVote = true;
-        p.closeVoteTimestamp =
-            block.timestamp +
-            g.getPolicy(policyName).voteDurationSecond;
-        p.policyWeight = policyWeight;
-        p.maxWeight = maxWeight;
-        p.minWeightOpenVote = minWeightOpenVote;
-        p.minWeightValidVote = minWeightValidVote;
-        p.minWeightApproveVote = minWeightApproveVote;
-        p.totalVoter = 0;
+    PolicyProposal storage p = policyProposals[addr];
+    p.policyIndex = policyIndex;
+    p.exists = true;
+    p.openVote = true;
+    p.closeVoteTimestamp =
+        block.timestamp + g.getPolicy(policyName).voteDurationSecond;
+    p.policyWeight = policyWeight;
+    p.maxWeight = maxWeight;
+    p.minWeightOpenVote = minWeightOpenVote;
+    p.minWeightValidVote = minWeightValidVote;
+    p.minWeightApproveVote = minWeightApproveVote;
+    p.totalVoter = 0;
+  }
+
+  function votePolicyProposal(address addr, bool approve) public {
+    require(policyProposals[addr].exists && policyProposals[addr].openVote,
+            "Error 4002: Policy proposal is closed or did not exists.");
+
+    PolicyProposal storage p = policyProposals[addr];
+
+    if (!p.voters[msg.sender].voted) {
+      p.votersAddress[p.totalVoter] = msg.sender;
+      p.totalVoter += 1;
+      p.voters[msg.sender].voted = true;
+      p.voters[msg.sender].approve = approve;
+    } else {
+      p.voters[msg.sender].approve = approve;
+    }
+  }
+
+  function processPolicyProposal(address addr) public {
+    PolicyProposal storage p = policyProposals[addr];
+    require(p.openVote, "Error 4003: Policy proposal was proceed.");
+
+    p.openVote = false;
+    Governance g = Governance(GovernanceContract);
+    ERC20 t = ERC20(g.getARATokenContract());
+    p.totalVoteToken = 0;
+    p.totalApproveToken = 0;
+    p.totalSupplyToken = t.totalSupply();
+
+    for (uint256 i = 0; i < p.totalVoter; i++) {
+      uint256 voterToken = t.balanceOf(p.votersAddress[i]);
+      p.totalVoteToken += voterToken;
+
+      if (p.voters[p.votersAddress[i]].approve) {
+        p.totalApproveToken += voterToken;
+      }
     }
 
-    function voteProposal(address addr, bool approve) public {
-        require(
-            proposals[addr].exists && proposals[addr].openVote,
-            "Error 4002: Proposal is closed or did not exists."
-        );
+    bool isVoteValid = p.totalVoteToken >=
+                       (p.totalSupplyToken *
+                        g.getPolicyByIndex(p.policyIndex).minWeightOpenVote) /
+                           g.getPolicyByIndex(p.policyIndex).maxWeight;
 
-        PolicyProposal storage p = proposals[addr];
+    bool isVoteApprove = p.totalApproveToken >=
+                         (p.totalVoteToken *
+                          g.getPolicyByIndex(p.policyIndex).minWeightOpenVote) /
+                             g.getPolicyByIndex(p.policyIndex).maxWeight;
 
-        if (!p.voters[msg.sender].voted) {
-            p.votersAddress[p.totalVoter] = msg.sender;
-            p.totalVoter += 1;
-            p.voters[msg.sender].voted = true;
-            p.voters[msg.sender].approve = approve;
-        } else {
-            p.voters[msg.sender].approve = approve;
-        }
+    p.voteResult = isVoteValid && isVoteApprove;
+
+    if (isVoteValid && isVoteApprove) {
+      g.setPolicyByProposal(p.policyIndex, addr, p.policyWeight, p.maxWeight,
+                            p.voteDurationSecond, p.minWeightOpenVote,
+                            p.minWeightValidVote, p.minWeightApproveVote);
     }
 
-    function processProposal(address addr) public {
-        PolicyProposal storage p = proposals[addr];
-        require(p.openVote, "Error 4003: Proposal was proceed.");
+    p.processResultTimestamp = block.timestamp;
+  }
 
-        p.openVote = false;
-        Governance g = Governance(GovernanceContract);
-        ERC20 t = ERC20(g.getARATokenContract());
-        p.totalVoteToken = 0;
-        p.totalApproveToken = 0;
-        p.totalSupplyToken = t.totalSupply();
+  function openManagerProposal(uint16 totalManager, address addr,
+                               ManagerInfo memory managers) public {
+    require(!managerProposals[addr].exists,
+            "Error 4004: Manager proposal address already exists.");
 
-        for (uint256 i = 0; i < p.totalVoter; i++) {
-            uint256 voterToken = t.balanceOf(p.votersAddress[i]);
-            p.totalVoteToken += voterToken;
+    Governance g = Governance(GovernanceContract);
+    ERC20 t = ERC20(g.getARATokenContract());
+    bytes8 policyIndex = g.stringToBytes8("MANAGERS_LIST");
 
-            if (p.voters[p.votersAddress[i]].approve) {
-                p.totalApproveToken += voterToken;
-            }
-        }
-
-        bool isVoteValid = p.totalVoteToken >=
-            (p.totalSupplyToken *
-                g.getPolicyByIndex(p.policyIndex).minWeightOpenVote) /
-                g.getPolicyByIndex(p.policyIndex).maxWeight;
-
-        bool isVoteApprove = p.totalApproveToken >=
-            (p.totalVoteToken *
-                g.getPolicyByIndex(p.policyIndex).minWeightOpenVote) /
-                g.getPolicyByIndex(p.policyIndex).maxWeight;
-
-        p.voteResult = isVoteValid && isVoteApprove;
-
-        if (isVoteValid && isVoteApprove) {
-            g.setPolicyByProposal(
-                p.policyIndex,
-                addr,
-                p.policyWeight,
-                p.maxWeight,
-                p.voteDurationSecond,
-                p.minWeightOpenVote,
-                p.minWeightValidVote,
-                p.minWeightApproveVote
-            );
-        }
-
-        p.processResultTimestamp = block.timestamp;
-    }
+    Require(t.balanceOf(msg.sender) >=
+                (t.totalSupply() * g.getPolicy(policyName).minWeightOpenVote) /
+                    g.getPolicy(policyName).maxWeight,
+            "Error 4005: Insufficient token to open manager proposal.");
+  }
 }
