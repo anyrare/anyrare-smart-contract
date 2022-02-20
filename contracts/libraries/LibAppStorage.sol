92331e742b60ed94dbc76fac4498cbc8c51cce33
Panasun Sunanta <panasunsunanta@gmail.com>
2022-02-20 21:15:08 +0700

    feat: app storage

--- contracts/libraries/LibAppStorage.sol
+++ contracts/libraries/LibAppStorage.sol
@@ -8,15 +8,15 @@ struct Member {
     uint8 accountType;
     string username;
     string thumbnail;
-    mapping(uint8 => address) multiSigAddresses;
     uint8 multiSigTotalAddress;
     uint8 multiSigTotalApprove;
-    mapping(uint32 => uint256) assets;
     uint32 totalAsset;
     uint256 totalBidAuction;
     uint256 totalWonAuction;
     uint32 totalFounderCollection;
     uint32 totalOwnCollection;
+    mapping(uint8 => address) multiSigAddresses;
+    mapping(uint32 => uint256) assets;
 }
 
 struct AssetAuction {
@@ -188,21 +188,51 @@ struct GovernanceVoter {
 }
 
 struct Governance {
+    uint16 totalFounder;
+    uint16 totalManager;
+    uint16 totalAdmin;
+    uint16 totalOperation;
+    uint256 founderMaxControlWeight;
+    uint256 managerMaxControlWeight;
+    uint256 operationMaxControlWeight;
     mapping(bytes32 => GovernancePolicy) policies;
     mapping(uint16 => GovernanceFounder) founders;
     mapping(address => uint16) foundersAddress;
     mapping(uint16 => GovernanceManager) managers;
     mapping(address => uint16) managersAddress;
+    mapping(uint16 => address) admins;
     mapping(uint16 => GovernanceOperation) operations;
     mapping(address => uint16) operationsAddress;
     mapping(address => GovernanceAuditor) auditors;
     mapping(address => GovernanceCustodian) custodians;
-    uint16 totalFounder;
-    uint16 totalManager;
-    uint16 totalOperation;
-    uint256 founderMaxControlWeight;
-    uint256 managerMaxControlWeight;
-    uint256 operationMaxControlWeight;
+}
+
+struct ManagementFund {
+    address governanceContract;
+    uint256 managementFundValue;
+    uint256 lockupFundValue;
+    uint256 lastDistributeFundTimestamp;
+    uint256 lastDistributeLockupFundTimestamp;
+    uint256 totalLockupFundSlot;
+    uint256 firstUnsettleLockupFundSlot;
+    mapping(uint256 => ManagementFundLockupFund) lockupFunds;
+}
+
+struct ManagementFundLockupFundList {
+    address addr;
+    uint256 amount;
+}
+
+struct ManagementFundLockupFund {
+    uint256 startingTotalARAValue;
+    uint256 targetTotalARAValue;
+    uint256 lastUnlockTotalARAValue;
+    uint256 remainLockup;
+    uint256 totalLockup;
+    uint256 prevUnsettleLockupFundSlot;
+    uint256 nextUnsettleLockupFundSlot;
+    uint16 totalList;
+    mapping(uint16 => ManagementFundLockupFundList) lists;
 }
 
 struct AppStorage {
@@ -212,6 +242,8 @@ struct AppStorage {
     uint256 totalMember;
     uint256 totalAsset;
     uint256 totalCollection;
+    ManagementFund managementFund;
+    Governance governance;
     mapping(address => Member) members;
     mapping(uint256 => Asset) assets;
     mapping(uint256 => Collection) collections;
