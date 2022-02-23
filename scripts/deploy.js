319a2c79b44f160b4cd3118a703b4189f76f4136
Panasun Sunanta <panasunsunanta@gmail.com>
2022-02-23 17:22:45 +0700

    fix: deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -37,95 +37,75 @@ const deployContract = async () => {
   const OwnershipFacet = await ethers.getContractFactory("OwnershipFacet");
   const MemberFacet = await ethers.getContractFactory("MemberFacet");
   const GovernanceFacet = await ethers.getContractFactory("GovernanceFacet");
-  const ARATokenFacet = await ethers.getContractFactory("ARATokenFacet",
-    {
-      libraries: {
-        LibBancorFormula: libBancorFormula.address,
-      },
-    }
-  );
+  const ARATokenFacet = await ethers.getContractFactory("ARATokenFacet", {
+    libraries: {
+      LibBancorFormula: libBancorFormula.address,
+    },
+  });
   const CollateralTokenFacet = await ethers.getContractFactory(
     "CollateralTokenFacet"
   );
 
   //add facet cut
   const facets = [
-    {
-      contract: await DiamondLoupeFacet.deploy(),
-      name: 'DiamondLoupeFacet'
-    },
-    {
-      contract: await OwnershipFacet.deploy(),
-      name: 'OwnershipFacet'
-    },
-    {
-      contract: await MemberFacet.deploy(),
-      name: 'MemberFacet'
-    },
-    {
-      contract: await GovernanceFacet.deploy(),
-      name: 'GovernanceFacet'
-    },
-    {
-      contract: await CollateralTokenFacet.deploy("wDAI", "wDAI"),
-      name: 'CollateralToken'
-    },
-    {
-      contract: await ARATokenFacet.deploy("ARA", "ARA"),
-      name: 'ARATokenFacet'
-    }
+    await DiamondLoupeFacet.deploy(),
+    await OwnershipFacet.deploy(),
+    await MemberFacet.deploy(),
+    await GovernanceFacet.deploy(),
+    await CollateralTokenFacet.deploy("wDAI", "wDAI"),
+    // await ARATokenFacet.deploy("ARA", "ARA"),
   ];
+
   const cuts = facets.map((r) => ({
     facetAddress: r.address,
     action: FacetCutAction.Add,
-    functionSelectors: getSelectors(r.contract),
+    functionSelectors: getSelectors(r),
   }));
 
   const diamondCut = await ethers.getContractAt("IDiamondCut", diamond.address);
   const functionCall = diamondInit.interface.encodeFunctionData("init");
-  console.log(diamondInit.address);
-  console.log(cuts);
-  console.log(functionCall);
+
   const tx = await diamondCut.diamondCut(
     cuts,
     diamondInit.address,
     functionCall
   );
-  // await tx.wait();
+  await tx.wait();
 
-  // const diamondLoupeFacet = await ethers.getContractAt("DiamondLoupeFacet", diamond.address);
-  // const ownershipFacet = await ethers.getContractAt("OwnershipFacet", diamond.address);
-  // const memberFacet = await ethers.getContractAt("MemberFacet", diamond.address);
-  // const governanceFacet = await ethers.getContractAt("GovernanceFacet", diamond.address);
-  // const araTokenFacet = await ethers.getContractAt("ARATokenFacet", diamond.address);
-  // const collateralTokenFacet = await ethers.getContractAt("CollateralTokenFacet", diamond.address);
-  // console.log("araTokenFacet");
+  const diamondLoupeFacet = await ethers.getContractAt("DiamondLoupeFacet", diamond.address);
+  const ownershipFacet = await ethers.getContractAt("OwnershipFacet", diamond.address);
+  const memberFacet = await ethers.getContractAt("MemberFacet", diamond.address);
+  const governanceFacet = await ethers.getContractAt("GovernanceFacet", diamond.address);
+  const collateralTokenFacet = await ethers.getContractAt("CollateralTokenFacet", diamond.address);
+  const araTokenFacet = await ethers.getContractAt("ARATokenFacet", diamond.address);
 
-  // await memberFacet.initMember();
+  console.log("araTokenFacet");
 
-  // await collateralTokenFacet.collateralTokenSetOwner(root.address);
-  // await collateralTokenFacet.collateralTokenMint(
-  //   root.address,
-  //   ethers.BigNumber.from("1" + "0".repeat(26))
-  // );
-  // await collateralTokenFacet.collateralTokenMint(
-  //   root.address,
-  //   ethers.BigNumber.from("1" + "0".repeat(26))
-  // );
+  await memberFacet.initMember();
+
+  await collateralTokenFacet.collateralTokenSetOwner(root.address);
+  await collateralTokenFacet.collateralTokenMint(
+    root.address,
+    ethers.BigNumber.from("1" + "0".repeat(26))
+  );
+  await collateralTokenFacet.collateralTokenMint(
+    root.address,
+    ethers.BigNumber.from("1" + "0".repeat(26))
+  );
   // await araTokenFacet.araTokenInitialize(
   //   collateralTokenFacet.address,
   //   ethers.BigNumber.from("1" + "0".repeat(26))
   // );
 
-  // return {
-  //   diamondAddress: diamond.address,
-  //   diamondLoupeFacet,
-  //   ownershipFacet,
-  //   memberFacet,
-  //   governanceFacet,
-  //   collateralTokenFacet,
-  //   araTokenFacet,
-  // };
+  return {
+    diamondAddress: diamond.address,
+    diamondLoupeFacet,
+    ownershipFacet,
+    memberFacet,
+    governanceFacet,
+    collateralTokenFacet,
+    // araTokenFacet,
+  };
 };
 
 if (require.main === module) {
